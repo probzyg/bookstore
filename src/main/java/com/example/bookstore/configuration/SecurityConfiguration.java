cc839ceeb65a5f9d90e1721fb2847fc378efcca6
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-18 20:09:52 +0300

    Created Spring Security configuration

---
+++ src/main/java/com/example/bookstore/configuration/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.bookstore.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("admin-api/books").authenticated()
+                        .anyRequest().permitAll());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
