2c80db18d300bc0586f534823b2a6a53a6f764ec
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-18 20:32:20 +0300

    Added method for adding books to DB

--- src/main/java/com/example/bookstore/controller/AdminController.java
+++ src/main/java/com/example/bookstore/controller/AdminController.java
@@ -1,9 +1,23 @@
 package com.example.bookstore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.bookstore.domain.Book;
+import com.example.bookstore.request.AddBookRequest;
+import com.example.bookstore.service.BookstoreDatabaseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/admin-api")
 public class AdminController {
+    private final BookstoreDatabaseService bookstoreDatabaseService;
+
+    public AdminController(BookstoreDatabaseService bookstoreDatabaseService) {
+        this.bookstoreDatabaseService = bookstoreDatabaseService;
+    }
+
+    @PutMapping("/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book addBook(@RequestBody AddBookRequest addBookRequest) {
+        return bookstoreDatabaseService.addBook(addBookRequest);
+    }
 }
