41c00b430587759c1676f4166714b0385567db84
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-18 20:15:00 +0300

    Added dependency for jakarta, created Book entity

---
+++ src/main/java/com/example/bookstore/domain/Book.java
@@ -0,0 +1,21 @@
+package com.example.bookstore.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Book {
+    @Id
+    @Column(name = "book_name")
+    private String bookName;
+    @Column(name = "author_name")
+    private String authorName;
+
+    public Book() {
+    }
+}
