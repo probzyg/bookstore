243c0a102f14fab05eb3821dc1d4d4af0ad05b9c
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-18 20:20:18 +0300

    Added database repository for book entity

---
+++ src/main/java/com/example/bookstore/repository/BookDatabaseRepository.java
@@ -0,0 +1,7 @@
+package com.example.bookstore.repository;
+
+import com.example.bookstore.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookDatabaseRepository extends JpaRepository<Book, String> {
+}
