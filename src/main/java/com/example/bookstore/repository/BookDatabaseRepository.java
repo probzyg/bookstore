0483ffe9c8d9e5b4fe14483f692364ddd25e846f
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 17:33:03 +0300

    Created custom query, returns all book names and prices, even if the price is NULL

--- src/main/java/com/example/bookstore/repository/BookDatabaseRepository.java
+++ src/main/java/com/example/bookstore/repository/BookDatabaseRepository.java
@@ -1,12 +1,17 @@
 package com.example.bookstore.repository;
 
 import com.example.bookstore.domain.Book;
+import com.example.bookstore.dto.BookDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BookDatabaseRepository extends JpaRepository<Book, String> {
     Book findBookByTitle(String title);
 
     Page<Book> findAllByOrderByCreatedAtDesc(Pageable pageable);
+
+    @Query("SELECT new com.example.bookstore.dto.BookDTO(b.title, b.price) FROM Book b WHERE b.price IS NOT NULL ORDER BY b.createdAt DESC")
+    Page<BookDTO> findAllBookWithPrices(Pageable pageable);
 }
