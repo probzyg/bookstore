7be5b0f02ccba4309bebcb8aa6226a74b40f3c16
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 15:19:59 +0300

    Modified Admin controller for calling of thymeleaf html scripts

--- src/main/java/com/example/bookstore/controller/AdminController.java
+++ src/main/java/com/example/bookstore/controller/AdminController.java
@@ -4,9 +4,11 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.request.AddBookRequest;
 import com.example.bookstore.service.BookstoreDatabaseService;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class AdminController {
     private final BookstoreDatabaseService bookstoreDatabaseService;
 
@@ -14,9 +16,17 @@ public class AdminController {
         this.bookstoreDatabaseService = bookstoreDatabaseService;
     }
 
-    @PutMapping("/add-book")
+    @GetMapping("/add-book")
+    public String showAddBookPage(Model model) {
+        model.addAttribute("addBookRequest", new AddBookRequest());
+        return "add-book";
+    }
+
+    @PostMapping("/add-book")
     @ResponseStatus(HttpStatus.CREATED)
-    public Book addBook(@RequestBody AddBookRequest addBookRequest) {
-        return bookstoreDatabaseService.addBook(addBookRequest);
+    public String addBook(@ModelAttribute AddBookRequest addBookRequest, Model model) {
+        Book newBook = bookstoreDatabaseService.addBook(addBookRequest);
+        model.addAttribute("newBook", newBook);
+        return "book-added";
     }
 }
