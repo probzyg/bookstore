09cc054261ac1311380754c848e2a0dea1095dc4
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 18:29:01 +0300

    Added price value to request object

--- src/main/java/com/example/bookstore/request/AddBookRequest.java
+++ src/main/java/com/example/bookstore/request/AddBookRequest.java
@@ -3,12 +3,15 @@ package com.example.bookstore.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class AddBookRequest {
     private String title;
     private String authorName;
     private String genre;
+    private BigDecimal price;
 
     public AddBookRequest() {
     }
