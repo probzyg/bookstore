d9774082c1ac31313ef64306482660f23116f2fc
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 15:33:21 +0300

    Added method for thymeleaf template call through controller

--- src/main/java/com/example/bookstore/controller/CustomerController.java
+++ src/main/java/com/example/bookstore/controller/CustomerController.java
@@ -2,12 +2,14 @@ package com.example.bookstore.controller;
 
 import com.example.bookstore.domain.Book;
 import com.example.bookstore.service.BookstoreDatabaseService;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class CustomerController {
     private final BookstoreDatabaseService bookstoreDatabaseService;
 
@@ -16,9 +18,15 @@ public class CustomerController {
     }
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
-    public List<Book> getBooksByPage(@RequestParam(name = "page", defaultValue = "1") int page,
-                                     @RequestParam(name = "size", defaultValue = "50") int size) {
-        return this.bookstoreDatabaseService.getBooksByPage(page, size);
+    public String showBookList(@RequestParam(name = "page", defaultValue = "1") int page,
+                               @RequestParam(name = "size", defaultValue = "50") int size,
+                               Model model) {
+        List<Book> books = bookstoreDatabaseService.getBooksByPage(page, size);
+        model.addAttribute("books", books);
+
+        Page<Book> bookPage = bookstoreDatabaseService.getBookPage(page, size);
+        model.addAttribute("pageInfo", bookPage);
+
+        return "books";
     }
 }
