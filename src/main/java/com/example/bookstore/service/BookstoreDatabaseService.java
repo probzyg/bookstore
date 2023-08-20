32b980c1879570d26f581c44938db141ed7847d5
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 18:38:07 +0300

    Improved validation of adding book request

--- src/main/java/com/example/bookstore/service/BookstoreDatabaseService.java
+++ src/main/java/com/example/bookstore/service/BookstoreDatabaseService.java
@@ -40,10 +40,10 @@ public class BookstoreDatabaseService {
         String title = addBookRequest.getTitle().trim();
         String authorName = addBookRequest.getAuthorName().trim();
         String genre = addBookRequest.getGenre().trim();
-        if (title == null || authorName == null || genre == null) {
-            return false;
+        if (title.equals("") || authorName.equals("") || genre.equals("")) {
+            return true;
         }
-        return true;
+        return false;
     }
 
     private Book createBook(AddBookRequest addBookRequest) {
