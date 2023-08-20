47dce88dba8b3843520df0fa07d262515c441fd2
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 18:26:17 +0300

    Integration of the thymeleaf to Ecom Controller

--- src/main/java/com/example/bookstore/controller/EcommerceController.java
+++ src/main/java/com/example/bookstore/controller/EcommerceController.java
@@ -3,6 +3,7 @@ package com.example.bookstore.controller;
 import com.example.bookstore.dto.BookDTO;
 import com.example.bookstore.service.BookstoreDatabaseService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +21,11 @@ public class EcommerceController {
     }
 
     @GetMapping
-    public List<BookDTO> getBooksWithNameAndPrice(@RequestParam(name = "page", defaultValue = "1") int page,
-                                                  @RequestParam(name = "size", defaultValue = "50") int size) {
-        return this.bookstoreDatabaseService.getAllBookWithPrices(page, size);
+    public String getBooksWithNameAndPrice(@RequestParam(name = "page", defaultValue = "1") int page,
+                                           @RequestParam(name = "size", defaultValue = "50") int size,
+                                           Model model) {
+        List<BookDTO> bookDTOs = this.bookstoreDatabaseService.getAllBookWithPrices(page, size);
+        model.addAttribute("books", bookDTOs);
+        return "ecom-books";
     }
 }
