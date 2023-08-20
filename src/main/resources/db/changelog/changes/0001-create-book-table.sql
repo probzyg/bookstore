6607f56276f6b742f96a02d198916d39085c3a38
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-20 17:25:34 +0300

    Modification of the sql script for creating books table in database

--- src/main/resources/db/changelog/changes/0001-create-book-table.sql
+++ src/main/resources/db/changelog/changes/0001-create-book-table.sql
@@ -6,5 +6,6 @@ create table book(
                      title varchar(255) PRIMARY KEY,
                      author_name varchar(255),
                      genre varchar(255),
+                     price DECIMAL(10, 2),
                      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
