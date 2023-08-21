06a4e1a8e16ef554175d1a755b0ccd3440f9f630
Jānis Kaveckis <kaveckisj@gmail.com>
2023-08-21 10:24:20 +0300

    Set price value in AddBookRequest class to BigDecimal

--- src/main/java/com/example/bookstore/request/AddBookRequest.java
+++ src/main/java/com/example/bookstore/request/AddBookRequest.java
@@ -3,13 +3,15 @@ package com.example.bookstore.request;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 public class AddBookRequest {
     private String title;
     private String authorName;
     private String genre;
-    private String price;
+    private BigDecimal price;
 
     public AddBookRequest() {
     }
